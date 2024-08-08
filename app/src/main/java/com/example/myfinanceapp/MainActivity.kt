9f5830dd11ad4efa33d9bf3581dbb41ede27f9cf
MUKHETHWA <masikngl@gmail.com>
2024-08-08 15:50:08 +0000

    updates by maxan3

--- app/src/main/java/com/example/myfinanceapp/MainActivity.kt
+++ app/src/main/java/com/example/myfinanceapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.myfinanceapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class MainActivity : TransactionActivity() {
     private lateinit var btnDeposit: Button
@@ -14,45 +16,59 @@ class MainActivity : TransactionActivity() {
     private lateinit var btnViewBalance: Button
     private lateinit var txtUserInfo: TextView
 
+    //card view variable declaration
+    private lateinit var cvDeposit:CardView
+    private lateinit var cvTransfer:CardView
+    private lateinit var cvWithdraw:CardView
+    private lateinit var cvVieTrans:CardView
+    private lateinit var cvBalance:CardView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Card view link with IDs
+         cvDeposit = findViewById(R.id.cvDeposit)
+        cvTransfer = findViewById(R.id.cvTrans)
+         cvWithdraw = findViewById(R.id.cvwith)
+        cvVieTrans = findViewById(R.id.cvHist)
+        cvBalance = findViewById(R.id.cvBalance)
 
-        btnDeposit = findViewById(R.id.btnDeposit)
+       /* btnDeposit = findViewById(R.id.btnDeposit)
         btnTransfer = findViewById(R.id.btnTransfer)
         btnWithdraw = findViewById(R.id.btnWithdraw)
         btnViewTransactions = findViewById(R.id.btnViewTransactions)
-        btnViewBalance = findViewById(R.id.btnViewBalance)
+        btnViewBalance = findViewById(R.id.btnViewBalance)*/
         txtUserInfo = findViewById(R.id.txtUserInfo)
 
         accountNumber = intent.getStringExtra("ACCOUNT_NUMBER") ?: ""
         displayUserInfo(txtUserInfo)  // Correct method call
 
-        btnDeposit.setOnClickListener {
+        cvDeposit.setOnClickListener {
             val intent = Intent(this, DepositActivity::class.java)
             intent.putExtra("ACCOUNT_NUMBER", accountNumber)
             startActivity(intent)
         }
 
-        btnTransfer.setOnClickListener {
+        cvTransfer.setOnClickListener {
             val intent = Intent(this, TransferActivity::class.java)
             intent.putExtra("ACCOUNT_NUMBER", accountNumber)
             startActivity(intent)
         }
 
-        btnWithdraw.setOnClickListener {
+        cvWithdraw.setOnClickListener {
             val intent = Intent(this, WithdrawActivity::class.java)
             intent.putExtra("ACCOUNT_NUMBER", accountNumber)
             startActivity(intent)
         }
 
-        btnViewTransactions.setOnClickListener {
+        cvVieTrans.setOnClickListener {
             val intent = Intent(this, TransactionHistoryActivity::class.java)
             intent.putExtra("ACCOUNT_NUMBER", accountNumber)
             startActivity(intent)
         }
 
-        btnViewBalance.setOnClickListener {
+        cvBalance.setOnClickListener {
             val intent = Intent(this, ViewBalanceActivity::class.java)
             intent.putExtra("ACCOUNT_NUMBER", accountNumber)
             startActivity(intent)
