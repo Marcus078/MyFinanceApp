a6f4275ba278e3bdf4ff66bcb6755b2616d5854b
Mukhethwa.Magadani <masikngl@gmail.com>
2024-08-15 11:07:16 +0000

    updates

--- app/src/main/java/com/example/myfinanceapp/Settings.kt
+++ app/src/main/java/com/example/myfinanceapp/Settings.kt
@@ -1,16 +1,30 @@
 package com.example.myfinanceapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Settings : AppCompatActivity() {
+
+    //button declaration
+    private lateinit var btnExit:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
+
+        //Assign id to the button
+        btnExit = findViewById(R.id.btnSettingExit)
+
+        //Onclick method for exit button
+        btnExit.setOnClickListener {
+           //code to exit the activity
+            finish()
+        }
 
     }
 }
